38cbe1e54d097fa6ed28d9442a38f9586553f233
Nortsova <anastasi.smch@gmail.com>
2016-11-13 12:46:02 +0200

    Merge commit 'e8932a3b251dd2f739cf51439ea492c7e81b8928'
    * commit 'e8932a3b251dd2f739cf51439ea492c7e81b8928':
      task3a
      task2c
      edit 2b task
      second task
      add task2A
      first
    
    # Conflicts:
    #	task3a

--- task3a/dist/index.es6.js
+++
@@ -1,5 +0,0 @@
-function hello(name) {
-  console.log('Hello ' + name); // eslint-disable-line
-}
-hello('JS World');
-//# sourceMappingURL=index.es6.js.map
--- task3a/dist/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-function hello(name) {
-  console.log('Hello ' + name); // eslint-disable-line
-}
-hello('JS World');
-//# sourceMappingURL=index.js.map
